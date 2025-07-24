a5f41879c1941cf8d7f7ea5c2f1429fc910b35f9
ehju <eh13.ju@samsung.com>
2025-07-24 15:34:57 +0900

    [feature] introduce mockDeviceDriver

--- DeviceDriver/main.cpp
+++ DeviceDriver/main.cpp
@@ -1,13 +1,27 @@
 #include "gmock/gmock.h"
 #include "device_driver.h"
+#include <vector>
+
+using namespace std;
+using namespace testing;
+class MockFlashMemory : public FlashMemoryDevice {
+public:
+	MOCK_METHOD(unsigned char, read, (long address), (override));
+	MOCK_METHOD(void, write, (long address, unsigned char data), (override));
+
+};
 
 TEST(DeviceDriver, ReadFromHW) {
-	// TODO : replace hardware with a Test Double
-	FlashMemoryDevice* hardware = nullptr;
-	DeviceDriver driver{ hardware };
+	MockFlashMemory mock;
+
+	EXPECT_CALL(mock, read(_))
+		.Times(5);
+
+	DeviceDriver driver{ &mock };
 	int data = driver.read(0xFF);
-	EXPECT_EQ(0, data);
 }
+
+
 
 int main() {
 	::testing::InitGoogleMock();
